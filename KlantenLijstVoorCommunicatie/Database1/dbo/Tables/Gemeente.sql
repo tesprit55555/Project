221d4e87eb2a701f6995b321c5cfd3aa3365a7dc
Thomas Esprit <thomasesprit@digi4u.onmicrosoft.com>
2019-11-22 18:05:40 +0100

    OPSLAG

---
+++ KlantenLijstVoorCommunicatie/Database1/dbo/Tables/Gemeente.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Gemeente] (
+    [IdGemeente] INT        IDENTITY (1, 1) NOT NULL,
+    [Gemeente]   NCHAR (10) NULL,
+    [Postcode]   INT        NULL,
+    PRIMARY KEY CLUSTERED ([IdGemeente] ASC)
+);
+
